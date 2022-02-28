1c3796e0c96432028249ef8059d2a41ca9b7ad71
JR <3142734033@qq.com>
2022-02-28 14:56:16 +0800

    讲师管理模块，统一异常处理和日志处理

--- service/service_edu/src/main/java/cn/jr/edu/controller/TeacherController.java
+++ service/service_edu/src/main/java/cn/jr/edu/controller/TeacherController.java
@@ -1,12 +1,17 @@
 package cn.jr.edu.controller;
 
 
+import cn.jr.ExceptionHandler.GuliException;
 import cn.jr.R;
 import cn.jr.edu.entity.Teacher;
+import cn.jr.edu.entity.vo.TeacherQuery;
 import cn.jr.edu.service.ITeacherService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -55,6 +60,73 @@ public class TeacherController {
         long total=teacherPage.getTotal();//获得总记录数
         List<Teacher> teachers = teacherPage.getRecords();//获得分页查询到的数据
         return R.ok().data("total",total).data("rows",teachers);
+    }
+
+
+    //条件分页查询
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageTeacherCondition(@PathVariable(value = "current") Long current
+            , @PathVariable(value = "limit") Long limit
+            , @RequestBody(required = false) TeacherQuery teacherQuery){
+
+        Page<Teacher> teacherPage=new Page<Teacher>(current,limit);
+
+        QueryWrapper<Teacher> queryWrapper = new QueryWrapper<>();
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+        if(!StringUtils.isEmpty(name)){
+            queryWrapper.like("name",name);
+        }
+        if(!StringUtils.isEmpty(level)){
+            queryWrapper.eq("level",level);
+        }
+        if(!StringUtils.isEmpty(begin)){
+            queryWrapper.ge("gmt_creat", begin);
+        }
+        if(!StringUtils.isEmpty(end)){
+            queryWrapper.le("gmt_creat",end);
+        }
+        teacherService.page(teacherPage,queryWrapper);
+        long total = teacherPage.getTotal();
+        List<Teacher> teachers = teacherPage.getRecords();
+        return R.ok().data("total",total).data("rows",teachers);
+    }
+
+    //添加讲师
+    @PostMapping("insertTeacher")
+    public R insertTeacher(@RequestBody Teacher teacher){
+        boolean flag=teacherService.save(teacher);
+        if (flag) {
+            return R.ok();
+        }else {
+            return R.error();
+        }
+    }
+
+    //根据id查询讲师
+    @GetMapping("selectTeacher/{id}")
+    public R selectById(@PathVariable(value = "id") String id){
+        try{
+            int i=10/0;
+        }catch (Exception e){
+            throw new GuliException(20001,"执行了自定义异常处理");
+        }
+        Teacher teacher = teacherService.getById(id);
+        return R.ok().data("teacher",teacher);
+    }
+
+    //根据id修改数据
+    @PostMapping("updateTeacher")
+    public R updateTeacher(@RequestBody Teacher teacher){
+
+        boolean flag=teacherService.updateById(teacher);
+        if (flag) {
+            return R.ok();
+        }else {
+            return R.error();
+        }
     }
 
 
